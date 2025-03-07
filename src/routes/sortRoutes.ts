9e08056cfd814ff054e44ebfef2e8feda776f46e
Ashish23jun <pandey.k.ashish.86@gmail.com>
2025-03-07 22:17:27 +0530

    Created All APis

--- src/routes/sortRoutes.ts
+++ src/routes/sortRoutes.ts
@@ -1,8 +1,14 @@
 import express from "express";
-import { getSortedTasks } from "../controllers/sortController";
+import {
+  getSortedTasks,
+  getTasksByPriority,
+  getTasksByStatus,
+} from "../controllers/sortController";
 
 const router = express.Router();
 
 router.get("/sortByTime", getSortedTasks);
+router.get("/filter/priority", getTasksByPriority);
+router.get("/filter/status", getTasksByStatus);
 
 export default router;
--- src/services/sortService.ts
+++ src/services/sortService.ts
@@ -10,3 +10,21 @@ export const getSortedTasksService = async (sortBy: string, order: string) => {
     orderBy: { [sortBy]: order === "desc" ? "desc" : "asc" },
   });
 };
+export const getTasksByPriority = async (priority: string) => {
+  const priorityNum = parseInt(priority);
+  if (isNaN(priorityNum) || priorityNum < 1 || priorityNum > 5) {
+    throw new Error("Invalid priority value. Use 1-5");
+  }
+
+  return await prisma.task.findMany({
+    where: { priority: priorityNum },
+  });
+};
+export const getTasksByStatus = async (status: string) => {
+  if (status !== "Pending" && status !== "Finished") {
+    throw new Error("Invalid status value. Use pending or finished");
+  }
+  return await prisma.task.findMany({
+    where: { status },
+  });
+};
