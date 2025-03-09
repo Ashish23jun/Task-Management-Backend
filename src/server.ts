09e1d739462aff31a0e8c289386126a7c57784a0
Ashish23jun <pandey.k.ashish.86@gmail.com>
2025-03-09 14:40:02 +0530

    cors fix

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import authRoutes from "./routes/authRoutes";
-import taskRoutes from "./routes/taskRoutes";
-import dashboardRoutes from "./routes/dashboardRoutes";
+import authRoutes from "./routes/authRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
+
 dotenv.config();
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://task-sigma-rust.vercel.app",  
+];
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, World! Backend is running!");
 });
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
